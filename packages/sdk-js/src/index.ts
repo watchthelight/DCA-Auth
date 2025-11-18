cb3eb2b0f67e7fffa511f06ff3d892d9eca58c7f
watchthelight <buteverythingisnormal@gmail.com>
2025-11-18 17:47:56 -0600

    Phase 14 (SDKs, Analytics Dashboard, multi-tenant features, WebSocket real-time features, plugin system, internationalization support)

---
+++ packages/sdk-js/src/index.ts
@@ -0,0 +1,9 @@
+export { DCAAuthClient } from './client';
+export { LicenseManager } from './modules/licenses';
+export { AuthManager } from './modules/auth';
+export { UserManager } from './modules/users';
+export { WebhookManager } from './modules/webhooks';
+export { RealtimeClient } from './modules/realtime';
+export * from './types';
+export * from './errors';
+export * from './utils';
